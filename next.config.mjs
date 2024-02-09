4a260562f366d359289d0d9c47271cc49738adaa
Naji Astier <na-ji@users.noreply.github.com>
2024-02-09 15:49:43 +0100

    chore: setup release management
    Setup semantic release to publish package versions and docker images.
    
    BREAKING CHANGE: Initial release

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+process.on('unhandledRejection', (error) => {
+  console.log('unhandledRejection', error);
+});
+
 export default nextConfig;
